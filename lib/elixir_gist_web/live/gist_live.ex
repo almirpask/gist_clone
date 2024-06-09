bf6e140dadff0a466fb091ae7d3468870908f055
Almir Santos <almirpask@hotmail.com>
2024-06-09 00:53:02 -0300

    add delete gist action

--- lib/elixir_gist_web/live/gist_live.ex
+++ lib/elixir_gist_web/live/gist_live.ex
@@ -7,4 +7,15 @@ defmodule ElixirGistWeb.GistLive do
     gist = Gists.get_gist!(id)
     {:ok, assign(socket, gist: gist)}
   end
+
+  def handle_event("delete", %{"id" => id}, socket) do
+    case Gists.delete_gist(socket.assigns.current_user, id) do
+      {:ok, _gist} ->
+        socket = put_flash(socket, :info, "Gist deleted successfully.")
+        {:noreply, push_navigate(socket, to: ~p"/create")}
+      {:error, message} ->
+        socket = put_flash(socket, :error, "Failed to delete gist: #{message}")
+        {:noreply, socket}
+    end
+  end
 end
